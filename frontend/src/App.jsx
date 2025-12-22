5986547a1d6f377a1e50d38b98d70cfe1eb10e42
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-12-22 18:15:40 +0530

    build Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
+import Navbar from "./components/layout/Navbar"
 
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="min-h-screen bg-slate-100">
+      <Navbar/>
+    </div>
   )
 }
 
